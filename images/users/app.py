cacc70d7936ad9e9c8cea0bd0ab3f614234cc953
Mirodil <ortikov.m97@gmail.com>
2024-09-30 23:54:34 +0500

    register blueprint

--- images/users/app.py
+++ images/users/app.py
@@ -1,31 +1,19 @@
-import os
-import logging
-from sys import exit
-
-# import api.v1
-# import defaults
-# import token_store
-from authlib.integrations.flask_client import OAuth
-from flask import Flask, jsonify
+from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_swagger_ui import get_swaggerui_blueprint
-from db import postgres
-# from storage.db_models import User
-
-
-logger = logging.getLogger(__name__)
     
 
 def create_app(app_config):
     app = Flask(__name__)
     app.config.from_object(app_config)
-    # app.config.from_object(defaults)
-    # logging.basicConfig(
-    #     level=app.config["LOG_LEVEL"],
-    # )
-
+    JWTManager(app)
+    from src.db import db
+    db.init_app(app)
+    import src.models
     with app.app_context():
-        postgres.init_db()
+        db.create_all()
 
+    from src.routes.v1 import router as main_blueprint
+    app.register_blueprint(main_blueprint, url_prefix='/api/v1')
+    print(app.url_map)
     return app
 
