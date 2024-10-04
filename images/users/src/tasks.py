ca5a9a187fc1ae9775935fd4784d0356afac86d1
Mirodil <ortikov.m97@gmail.com>
2024-10-04 11:02:18 +0500

    print removed

--- images/users/src/tasks.py
+++ images/users/src/tasks.py
@@ -1,7 +1,8 @@
 # from celery import shared_task
 from src.services.sms import SMSService
-from ..celery_app import celery
+from celery import shared_task
 
-@celery.task(name="send_security_code_task")
+@shared_task()
 def send_security_code_task(phone_number: str):
+    print("TASK CAlled")
     SMSService.send_code_by_telegram(phone_number)
