478739266c2020c63b976489cb0206882fd40d80
Mirodil <ortikov.m97@gmail.com>
2024-10-04 17:27:39 +0500

    typo

--- images/users/src/services/user.py
+++ images/users/src/services/user.py
@@ -1,17 +1,15 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from src.models import User
 from src.db import db
 from src.exceptions import UnknownUser
+from src.models import User
 
 
 class UserService:
 
     @staticmethod
     def get_user_by_phone_number(phone_number):
-        user = User.query.filter_by(email=phone_number).first()
-        if not user:
-            raise UnknownUser
+        user = User.query.filter_by(phone_number=phone_number).first()
         return user
 
     @staticmethod
@@ -25,7 +23,7 @@ class UserService:
     def create_user(phone_number, password):
         user = User(phone_number=phone_number, password=password)
         user.password = generate_password_hash(
-            user.password, method='pbkdf2:sha256:5', salt_length=8
+            user.password, method="pbkdf2:sha256:5", salt_length=8,
         )
         db.session.add(user)
         db.session.commit()
@@ -35,17 +33,17 @@ class UserService:
         user = cls.get_user_by_id(user_id)
 
         for key, value in user_new_data.items():
-            if key == 'password':
+            if key == "password":
                 setattr(
                     user,
                     key,
                     generate_password_hash(user_new_data[key],
-                    method='pbkdf2:sha256:5', salt_length=8)
+                    method="pbkdf2:sha256:5", salt_length=8)
                 )
-            elif key not in ['id', 'is_premium']:
+            elif key not in ["id", "is_premium"]:
                 setattr(user, key, user_new_data[key])
             else:
-                raise Exception('insufficient privileges')
+                raise Exception("insufficient privileges")
         db.session.add(user)
         db.session.commit()
 
