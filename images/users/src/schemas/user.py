8147fe2165640a855a72161f40b20cdb9e27f203
Mirodil <ortikov.m97@gmail.com>
2024-10-04 22:22:01 +0500

    new field

--- images/users/src/schemas/user.py
+++ images/users/src/schemas/user.py
@@ -4,4 +4,5 @@ from marshmallow import Schema, fields
 class UserSchema(Schema):
     phone_number = fields.String(required=True)
     password = fields.String(required=True, load_only=True)
+    role = fields.String(required=True)
     code = fields.String(required=True)
