af993d69f4ea5016f74c7571edf48dbbff04460f
Mirodil <ortikov.m97@gmail.com>
2024-10-04 11:01:29 +0500

    sms service

--- images/users/src/services/sms.py
+++ images/users/src/services/sms.py
@@ -1,3 +1,4 @@
+import random
 import telebot
 
 from src.utils import generate_security_code
@@ -12,11 +13,14 @@ bot = telebot.TeleBot(
 class SMSService:
     @staticmethod
     def send_code_by_telegram(phone_number: str = None):
-        print("SENDING")
-        code = generate_security_code(6)
+        code = generate_security_code()
         redis_db.setex(phone_number, 60, code)
 
         bot.send_message(
             chat_id=-1001541588192,
-            text=f"Используйте код {code} для авторизации",
-        )
+            text=f"Используйте код <strong>132566<strong/> для авторизации",
+        ) 
+    @staticmethod
+    def verify_code(phone_number: str):
+        code = redis_db.get(phone_number)
+        print(code)
