b1b39968caf35e14164e4f77ba7a7744ac702852
Mirodil <ortikov.m97@gmail.com>
2024-10-04 17:27:14 +0500

    sms service methods added

--- images/users/src/services/sms.py
+++ images/users/src/services/sms.py
@@ -1,26 +1,28 @@
-import random
 import telebot
 
-from src.utils import generate_security_code
 from src.cache import redis_db
+from src.utils import generate_security_code
 
 bot = telebot.TeleBot(
     "5393375054:AAFCLPFARn3GLIyZp_eI1c8YIWOvnpwda7s",
-    parse_mode="HTML"
+    parse_mode="HTML",
 )
 
 
 class SMSService:
     @staticmethod
-    def send_code_by_telegram(phone_number: str = None):
+    def send_code_by_telegram(phone_number: str):
         code = generate_security_code()
-        redis_db.setex(phone_number, 60, code)
-
-        bot.send_message(
-            chat_id=-1001541588192,
-            text=f"Используйте код <strong>132566<strong/> для авторизации",
-        ) 
-    @staticmethod
-    def verify_code(phone_number: str):
-        code = redis_db.get(phone_number)
+        redis_db.setex(phone_number, 60, str(code))
         print(code)
+        # bot.send_message(
+        #     chat_id=-1001541588192,
+        #     text=f"Используйте код <strong>132566<strong/> для авторизации",
+        # )
+
+    @staticmethod
+    def verify_code(phone_number: str, security_code: str):
+        code = redis_db.get(phone_number)
+        if code == security_code:
+            return True
+        return None
