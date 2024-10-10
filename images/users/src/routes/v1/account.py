4b7ffec686d659218ed445e67433c500a5059461
Mirodil <ortikov.m97@gmail.com>
2024-10-10 17:29:29 +0500

    profile route

--- images/users/src/routes/v1/account.py
+++ images/users/src/routes/v1/account.py
@@ -1,27 +1,16 @@
-import json
-import random
-import string
-from urllib.parse import urlencode
-from urllib.request import urlopen
-
-from celery import shared_task
 from flask import request
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from marshmallow import ValidationError
 
 from src import schemas
 from src.routes.v1 import router
-from src.services.device import DeviceService
-from src.services.history import HistoryService
-from src.services.sms import SMSService
-from src.services.token import TokenService
 from src.services.user import UserService
-from src.tasks import send_security_code_task
 
 
 @router.route("/get/<int:user_id>", methods=["GET"])
 def get_user_account(user_id):
     user = UserService.get_user_by_id(user_id=user_id)
-    user_roles = [{"id": role.id, "name": role.role_name} for role in user.roles]
+    user_roles = ",".join([role.role_name for role in user.roles])
 
     return {
         "avatar": user.avatar,
@@ -34,3 +23,41 @@ def get_user_account(user_id):
         "is_premium": user.is_premium,
         "roles": user_roles,
     }, 200
+
+
+@router.route("/me", methods=["GET"])
+@jwt_required(fresh=True)
+def get_me():
+    user = UserService.get_user_by_id(user_id=get_jwt_identity())
+    user_roles = ",".join([role.role_name for role in user.roles])
+
+    return {
+        "avatar": user.avatar,
+        "phone_number": user.phone_number,
+        "web_link": user.web_link,
+        "facebook_link": user.facebook_link,
+        "insta_link": user.insta_link,
+        "telegram_link": user.telegram_link,
+        "is_verified_by_admin": user.is_verified_by_admin,
+        "is_premium": user.is_premium,
+        "roles": user_roles,
+    }, 200
+
+
+@router.route("/update", methods=["PUT"])
+@jwt_required(fresh=True)
+def update_user_account():
+    user_data = request.json
+    user_id = get_jwt_identity()
+
+    try:
+        schemas.UserUpdateSchema().load(user_data)
+    except ValidationError as e:
+        return {"error": "validation error", "detail": e.messages}, 400
+
+    user = UserService.get_user_by_id(user_id=user_id)
+    UserService.update_user(
+        user_id=user_id,
+        **user_data,
+    )
+    return {"error": "no error", "detail": "Account updated successfully"}, 200
