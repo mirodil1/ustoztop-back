492a512e9ed96cdb5b81725002cc1ca462426d37
Mirodil <ortikov.m97@gmail.com>
2024-10-15 22:06:47 +0500

    error handlers

---
+++ images/users/src/routes/v1/error_handlers.py
@@ -0,0 +1,53 @@
+from flask import jsonify
+from marshmallow.exceptions import ValidationError
+from werkzeug.exceptions import BadRequest
+
+from src.exceptions import (DeviceAlreadyExists, InvalidEmail, InvalidRefreshToken,
+                      RoleAlreadyExists, UnknownDevice, UnknownUser)
+
+from . import router
+
+
+@router.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return jsonify({"error": e.description}), e.code
+
+
+@router.errorhandler(ValidationError)
+def handle_validation_error(e):
+    return {"error": e.messages}, 400
+
+
+@router.errorhandler(InvalidRefreshToken)
+def handle_invalid_refresh_token(e):
+    return {"error": "invalid refresh token"}, 422
+
+
+@router.errorhandler(UnknownDevice)
+def handle_unknown_device(e):
+    return {"error": "unknown device"}, 422
+
+
+@router.errorhandler(DeviceAlreadyExists)
+def handle_device_already_exists(e):
+    return {"error": "the device has been already activated"}, 422
+
+
+@router.errorhandler(RoleAlreadyExists)
+def handle_role_already_exists(e):
+    return {"error": "role already exists"}, 422
+
+
+@router.errorhandler(InvalidEmail)
+def handle_invalid_email(e):
+    return {"error": "invalid email"}, 422
+
+
+@router.errorhandler(UnknownUser)
+def handle_unknown_user(e):
+    return {"error": "unknown user"}, 422
+
+
+@router.errorhandler(429)
+def handle_unknown_user(e):
+    return {"error": "too many requests, try again later"}, 429
