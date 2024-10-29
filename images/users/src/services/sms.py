b7588878b4784efb61408c7e0be259c48665a5ad
Mirodil <ortikov.m97@gmail.com>
2024-10-29 16:44:37 +0500

    send code

--- images/users/src/services/sms.py
+++ images/users/src/services/sms.py
@@ -14,11 +14,10 @@ class SMSService:
     def send_code_by_telegram(phone_number: str):
         code = generate_security_code()
         redis_db.setex(phone_number, 60, str(code))
-        print(code)
-        # bot.send_message(
-        #     chat_id=-1001541588192,
-        #     text=f"Используйте код <strong>132566<strong/> для авторизации",
-        # )
+        bot.send_message(
+            chat_id=-1001541588192,
+            text=f"Используйте код <strong>{code}</strong> для авторизации",
+        )
 
     @staticmethod
     def verify_code(phone_number: str, security_code: str):
--- images/users/src/tasks.py
+++ images/users/src/tasks.py
@@ -1,8 +1,9 @@
 # from celery import shared_task
-from src.services.sms import SMSService
 from celery import shared_task
+
+from src.services.sms import SMSService
+
 
 @shared_task()
 def send_security_code_task(phone_number: str):
-    print("TASK CAlled")
     SMSService.send_code_by_telegram(phone_number)
