5b4301d4f9e4095588cf11e052f3de943a1dc72e
Mirodil <ortikov.m97@gmail.com>
2025-02-10 23:06:52 +0500

    cors

--- images/statistics/main.py
+++ images/statistics/main.py
@@ -21,7 +21,6 @@ app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
     allow_headers=["*"],
 )
 
