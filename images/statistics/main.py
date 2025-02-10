0c31937b59e62ed1a39b6578f08daa97b74a44f0
Mirodil <ortikov.m97@gmail.com>
2025-02-10 22:49:48 +0500

    CORS

--- images/statistics/main.py
+++ images/statistics/main.py
@@ -2,12 +2,14 @@
 import logging
 
 import uvicorn as uvicorn
-from core.config import settings
-from core.logger import LOGGING
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+
 from src.api import v1
 from src.middlewares import JWTAuthBackend
-from starlette.middleware.authentication import AuthenticationMiddleware
+from core.config import settings
+from core.logger import LOGGING
 
 app = FastAPI(
     title=settings.project_name,
@@ -16,6 +18,13 @@ app = FastAPI(
 )
 
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(v1.router, prefix="/api/v1/statistics")
 
 if __name__ == "__main__":
