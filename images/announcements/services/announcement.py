d167c9d1002cd47d70a8111c040a0aca4ceb49f5
Mirodil <ortikov.m97@gmail.com>
2025-02-28 10:59:35 +0500

    endpoint fixed

--- images/announcements/services/announcement.py
+++ images/announcements/services/announcement.py
@@ -45,7 +45,7 @@ class AnnouncementService:
         if user_gender or role_name:
             async with httpx.AsyncClient() as client:
                 response = await client.get(
-                    f"{settings.user_url}/api/v1/users/get/idx?gender={user_gender if user_gender else ''}&role={role_name if user_gender else ''}",
+                    f"{settings.user_url}/api/v1/users/get/idx?gender={user_gender if user_gender else ''}&role={role_name if role_name else ''}",
                 )
                 users = response.json()
                 user_ids = []
