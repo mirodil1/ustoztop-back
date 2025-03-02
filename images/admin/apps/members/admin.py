a3fac3d376a48ac1e91466bc32d55027dbace296
Mirodil <ortikov.m97@gmail.com>
2025-03-02 13:55:48 +0500

    db name for using

--- images/admin/apps/members/admin.py
+++ images/admin/apps/members/admin.py
@@ -9,28 +9,28 @@ class MultiDBModelAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         # Tell Django to save objects to the 'members' database.
-        obj.save(using=self.using)
+        obj.save(using="members")
 
     def delete_model(self, request, obj):
         # Tell Django to delete objects from the 'members' database
-        obj.delete(using=self.using)
+        obj.delete(using="members")
 
     def get_queryset(self, request):
         # Tell Django to look for objects on the 'members' database.
-        return super().get_queryset(request).using(self.using)
+        return super().get_queryset(request).using("members")
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         # Tell Django to populate ForeignKey widgets using a query
         # on the 'members' database.
         return super().formfield_for_foreignkey(
-            db_field, request, using=self.using, **kwargs
+            db_field, request, using="members", **kwargs
         )
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         # Tell Django to populate ManyToMany widgets using a query
         # on the 'members' database.
         return super().formfield_for_manytomany(
-            db_field, request, using=self.using, **kwargs
+            db_field, request, using="members", **kwargs
         )
 
 
@@ -39,20 +39,20 @@ class MultiDBTabularInline(admin.StackedInline):
 
     def get_queryset(self, request):
         # Tell Django to look for inline objects on the 'members' database.
-        return super().get_queryset(request).using(self.using)
+        return super().get_queryset(request).using("members")
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         # Tell Django to populate ForeignKey widgets using a query
         # on the 'members' database.
         return super().formfield_for_foreignkey(
-            db_field, request, using=self.using, **kwargs
+            db_field, request, using="members", **kwargs
         )
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         # Tell Django to populate ManyToMany widgets using a query
         # on the 'members' database.
         return super().formfield_for_manytomany(
-            db_field, request, using=self.using, **kwargs
+            db_field, request, using="members", **kwargs
         )
 
     def has_change_permission(self, request, obj = ...):
