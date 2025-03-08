4acb8fec9d4e2cd95f7be46340f33f59342c1d85
Mirodil <ortikov.m97@gmail.com>
2025-03-08 17:22:47 +0500

    default balance

--- images/users/src/services/wallet.py
+++ images/users/src/services/wallet.py
@@ -5,7 +5,7 @@ from src.models import Wallet
 class WalletService:
 
     @staticmethod
-    def create_wallet(user_id):
-        wallet = Wallet(user_id=user_id)
+    def create_wallet(user_id, balance=100000):
+        wallet = Wallet(user_id=user_id, balance=balance)
         db.session.add(wallet)
         db.session.commit()
