eab5af8c19d53715a1843ce1b305fa010498e449
Mirodil <ortikov.m97@gmail.com>
2025-03-26 16:22:15 +0500

    region admin

--- images/admin/apps/core/admin.py
+++ images/admin/apps/core/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Region
 
+class RegionInline(admin.StackedInline):
+    model = Region
+    extra = 10
+
+
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ["uz"]
     search_fields = [
         "uz", "ru"
     ]
+    inlines = [RegionInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(parent__isnull=True)
